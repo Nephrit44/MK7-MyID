cef9c096886420d75709e95bd2f6f02c55835c3b
Nephrit44 <nephrit44@yandex.ru>
2025-10-01 16:13:20 +0300

    Предрелиз

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ const listAndroid = document.querySelector('.list-android')
 
 btnApple.addEventListener('click', function () {
     listApple.classList.toggle('visible');
+    listAndroid.classList.add('visible');
 }); 
 
 btnAndroid.addEventListener('click', function(){
     listAndroid.classList.toggle('visible');
+    listApple.classList.add('visible');
 });
