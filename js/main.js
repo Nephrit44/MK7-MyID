13cddbf066e06049360f2f2ca3d26d4f48acf7fc
Nephrit44 <nephrit44@yandex.ru>
2025-10-01 22:48:03 +0300

    Добавлен счётчик

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 const btnApple = document.querySelector('.ios-icon');
 const btnAndroid = document.querySelector('.android-icon');
 const listApple = document.querySelector('.list-iphone');
-const listAndroid = document.querySelector('.list-android')
+const listAndroid = document.querySelector('.list-android');
+const counterApple = document.querySelectorAll('.counter-apple');
+const counterAndroid = document.querySelectorAll('.counter-android');
 
 btnApple.addEventListener('click', function () {
     listApple.classList.toggle('visible');
     listAndroid.classList.add('visible');
+    generateCounter(counterApple);
 }); 
 
 btnAndroid.addEventListener('click', function(){
     listAndroid.classList.toggle('visible');
     listApple.classList.add('visible');
+    generateCounter(counterAndroid);
 });
+
+
+function generateCounter (listCollection){
+    let i = 1;
+    listCollection.forEach(element => {
+        element.textContent = i;
+        i++;
+    });
+}
