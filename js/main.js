179e166b84053283bb105198b75f041f5e0bb872
Nephrit44 <nephrit44@yandex.ru>
2025-10-01 15:25:23 +0300

    Первый макет инструкции

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const btnApple = document.querySelector('.ios-icon');
+const btnAndroid = document.querySelector('.android-icon');
+const listApple = document.querySelector('.list-iphone');
+const listAndroid = document.querySelector('.list-android')
+
+btnApple.addEventListener('click', function () {
+    listApple.classList.toggle('visible');
+}); 
+
+btnAndroid.addEventListener('click', function(){
+    listAndroid.classList.toggle('visible');
+});
